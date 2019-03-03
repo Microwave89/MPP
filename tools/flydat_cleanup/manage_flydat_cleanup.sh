8891d0386ac0fca206fb6699c2079b22e36b0c8a
Microwave89 <teslahv@gmail.com>
2019-03-03 21:43:45 +0100

    Add files via upload

---
+++ tools/flydat_cleanup/manage_flydat_cleanup.sh
@@ -0,0 +1,25 @@
+#!/system/bin/sh
+FLYDAT_CLEANUP_LOG=/data/dji/log/custom_log.txt
+GND_IP_ADDR=192.168.41.2
+
+# Make sure nnn number is always reset after cleanup
+# echo>/blackbox/flyctrl/FLY001.DAT
+# mount -o bind 2>>$FLYDAT_CLEANUP_LOG /vendor/cfg/my_FLY001.DAT /blackbox/flyctrl/FLY001.DAT
+mount>>$FLYDAT_CLEANUP_LOG
+
+busybox ping -c 1 -w $(($((1 << 31)) - 1)) $GND_IP_ADDR>>$FLYDAT_CLEANUP_LOG
+
+while :
+do
+	sleep 0.1
+	# Redirect stderr (2) to /dev/null to prevent "LOG: write failed" messages clogging stdout
+	logcat 2>/dev/null | grep -FEm 1 ', 0, 1, 0, 1, 0, 0, ., 1, 0, 0, ., 0, 0, 0, ..'>>$FLYDAT_CLEANUP_LOG
+	echo "\"Pause\" + \"C2\" buttons pressed ==> Removing all FLYnnn.DAT!">>$FLYDAT_CLEANUP_LOG
+	rm -rf 2>>$FLYDAT_CLEANUP_LOG /blackbox/flyctrl/
+	touch /blackbox/flyctrl/FLY002.DAT
+
+	echo "Waiting for button release...">>$FLYDAT_CLEANUP_LOG
+
+	logcat 2>/dev/null | grep -FEm 1 ', 0, 0, 0, 0, 0, 0, ., 1, 0, 0, ., 0, 0, 0, ..'>>$FLYDAT_CLEANUP_LOG
+	echo "Buttons released!">>$FLYDAT_CLEANUP_LOG
+done
