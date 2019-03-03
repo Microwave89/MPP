d6d0e59b90ace3cb47e3cb0db3bfb8f2549d0df6
Microwave89 <teslahv@gmail.com>
2019-03-03 21:50:03 +0100

    Update manage_sdr_power.sh

--- tools/jkson_mod/manage_sdr_power.sh
+++ tools/jkson_mod/manage_sdr_power.sh
@@ -8,19 +8,20 @@ while :
 do
 	sleep 0.1
 	# Redirect stderr (2) to /dev/null to prevent "LOG: write failed" messages clogging stdout
-	logcat 2>/dev/null | grep -FEm 1 ', 0, 1, 1, 1, 0, 0, ., 1, 0, 0, ., 0, 0, 0, ..'>>$SDR_POWER_LOG
+	logcat 2>/dev/null | grep -FEm 1 ', 0, 1, 1, 1, 0, 0, ., ., 0, 0, ., 0, 0, 0, ..'>>$SDR_POWER_LOG
 	echo "\"Pause\" + \"C1\" + \"C2\" buttons pressed ==> Setting SDR power to \"FCC\"!">>$SDR_POWER_LOG
 	dji_mb_ctrl -S test -R local -g 9 -s 9 -c 27 00024800FFFF0200000000>>$SDR_POWER_LOG
 	echo "Waiting for button release...">>$SDR_POWER_LOG
 
-	logcat 2>/dev/null | grep -FEm 1 ', 0, 0, 0, 0, 0, 0, ., 1, 0, 0, ., 0, 0, 0, ..'>>$SDR_POWER_LOG
+	logcat 2>/dev/null | grep -FEm 1 ', 0, 0, 0, 0, 0, 0, ., ., 0, 0, ., 0, 0, 0, ..'>>$SDR_POWER_LOG
 	echo "Buttons released!">>$SDR_POWER_LOG
 
+	# SDR power may only be reset while in default flight mode
 	logcat 2>/dev/null | grep -FEm 1 ', 0, 1, 1, 1, 0, 0, ., 1, 0, 0, ., 0, 0, 0, ..'>>$SDR_POWER_LOG
 	echo "\"Pause\" + \"C1\" + \"C2\" buttons pressed ==> Setting SDR power to default!">>$SDR_POWER_LOG
 	dji_mb_ctrl -S test -R local -g 9 -s 9 -c 27>>$SDR_POWER_LOG
 	echo "Waiting for button release...">>$SDR_POWER_LOG
 
-	logcat 2>/dev/null | grep -FEm 1 ', 0, 0, 0, 0, 0, 0, ., 1, 0, 0, ., 0, 0, 0, ..'>>$SDR_POWER_LOG
+	logcat 2>/dev/null | grep -FEm 1 ', 0, 0, 0, 0, 0, 0, ., ., 0, 0, ., 0, 0, 0, ..'>>$SDR_POWER_LOG
 	echo "Buttons released!">>$SDR_POWER_LOG
 done
